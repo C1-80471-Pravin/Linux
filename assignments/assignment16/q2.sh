0a69ef493185d046cdb79a8f64cc9f3054c3ae7c
C1-80471 <pravinsawant9869@gmail.com>
2023-10-25 15:47:53 +0530

    assignment

--- assignments/assignment16/q2.sh
+++ assignments/assignment16/q2.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
+echo "Enter 1 for date,2for calander,3 for ls,4 for pwd ,5 exit"
 
+read ui
+
+if [ $ui -eq 1 ]
+then
+date
+elif [ $ui -eq 2 ]
+then
+cal
+elif [ $ui -eq 3 ]
+then
+ls
+elif [ $ui -eq 4 ]
+then
+pwd
+elif [ $ui -eq 5 ]
+then
+echo "Exit"
+else
+echo "Enter a valid number"
+fi
--- classwork/demo06.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo -n "enter radius of circle:"
-read radius
-
-area=`echo "3.142 * $radius * $radius" | bc`
-
-echo "area of circle = $area"
